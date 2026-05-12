40dcb7045b4895b23e196d6868e5580b5c621771
Raj0064 <nandanrajdeep2022@gmail.com>
2026-05-12 23:23:03 +0530

    Updated Dependencies

--- src/pages/admin/AdminResults.jsx
+++ src/pages/admin/AdminResults.jsx
@@ -2,7 +2,7 @@ import { useEffect, useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import StatCard from "../../components/admin/StatCard";
-import Button from "../../components/ui/button";
+import Button from "../../components/ui/Button";
 
 import { getExams } from "../../services/exam.service";
 import {
